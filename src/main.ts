d7d2118ee9f97d351baff0dd3b32504aa9c95eb7
tkgka <ksh17171@gmail.com>
2022-06-18 14:20:59 +0900

    feat(update): updated graphql max size

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'express';
 const DBConnect = require('./DB/index');
 DBConnect();
 
@@ -8,6 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.getHttpAdapter().getInstance().set('etag', false);
   app.disable('x-powered-by')
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.enableCors();
   await app.listen(3000);
 }
