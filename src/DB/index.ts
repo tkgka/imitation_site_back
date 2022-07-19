20878d80970f80c7306b4f539bfc7feab7f79021
tkgka <ksh17171@gmail.com>
2022-07-19 12:56:28 +0900

    feat(update):updated DBUser

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const dotenv = require('dotenv').config();
 
-const MONGO_URL = `mongodb+srv://root:${process.env.DBpasswd}@cluster0.ywtqd.mongodb.net/${process.env.database}?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${process.env.DBUser}:${process.env.DBpasswd}@cluster0.ywtqd.mongodb.net/${process.env.database}?retryWrites=true&w=majority`;
 
 module.exports = () => {
   mongoose
